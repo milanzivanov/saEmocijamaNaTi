df72b2acb2141e6748c3af60d84c65c8782e1efe
milanzivanov <milan.zivanov@gmail.com>
2024-03-04 08:38:07 +0100

    inprovments

--- src/js/main.js
+++ src/js/main.js
@@ -32,7 +32,6 @@ mobileNavigation.addEventListener("click", function () {
 document.addEventListener("keydown", (e) => {
   if (e.key === "Escape") {
     document.querySelector(".nav-container-mobile").classList.remove("active");
-    // document.querySelector(".nav-toggle-btn").classList.remove("active");
   }
 });
 
@@ -98,4 +97,34 @@ document.querySelector(".nav-list").addEventListener("click", function (e) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
+});
+
+///////////////////////
+// lazy loading with intersectionObserver
+const images = document.querySelectorAll("img[data-src]");
+
+function preloadImage(img) {
+  const src = img.getAttribute("data-src");
+  if (!src) {
+    return;
+  }
+  img.src = src;
+}
+const imgOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px 200px 0px"
+};
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  });
+}, imgOptions);
+
+images.forEach((image) => {
+  imgObserver.observe(image);
 });
