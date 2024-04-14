3e43f318675208d0d93b1abe4ab7aa74aabe0114
milanzivanov <milan.zivanov@gmail.com>
2024-04-14 14:58:47 +0200

    adding some inprovments

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
-// import Swiper bundle with all modules installed
 import Swiper from "swiper/bundle";
-// import styles bundle
 import "swiper/css/bundle";
-
 import "../scss/style.scss";
 
 // Initialize Swiper
